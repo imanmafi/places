efc5ecfcd550f4e506f19bd73cf16310f419674d
Toma Apostolov & Iman Mafi <student@epicodus.com>
2015-08-11 14:30:54 -0700

    pass specs for - create a class method that is and empty array and then save to it.

--- spec/places_spec.rb
+++ spec/places_spec.rb
@@ -9,3 +9,17 @@ require('places')
       end
     end
   end
+
+  describe(".all") do
+    it("start empty") do
+      expect(Place.all()).to(eq([]))
+    end
+  end
+
+  describe("#save") do
+    it("saves the place in the array of places") do
+      test_location = Place.new("New York")
+      test_location.save()
+      expect(Place.all()).to(eq([test_location]))
+    end
+  end
